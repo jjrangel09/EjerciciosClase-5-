476a8859fcc50d0d01ca92fd9a6b2aa766ba368e
Juan José Rangel <juanjose840@yahoo.com.co>
2018-02-20 09:36:37 -0500

    Auto stash before merge of "master" and "origin/master"

--- EjercicioClase1/src/ejercicioclase1/EjercicioClase1.java
+++ EjercicioClase1/src/ejercicioclase1/EjercicioClase1.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ejercicioclase1;
-
+import java.util.Scanner;
 /**
  *
  * @author JuanJ
@@ -16,6 +16,29 @@ public class EjercicioClase1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String nombre;
+        String desea = "si";
+        int cantN = 0;
+        int nota;
+        int suma = 0;
+        float prom;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el nombre del Estudiante: ");
+	nombre = sc.nextLine();
+        
+        do{
+            cantN++;
+            Scanner notas = new Scanner(System.in);
+            System.out.println("Introduzca nota: ");
+            nota = sc.nextInt();
+            suma = suma+nota;
+            Scanner deseo = new Scanner(System.in);
+            System.out.println("Desea agregar nota? (si/no)");
+	    desea = deseo.nextLine();
+        }while("si".equals(desea));
+        prom=suma/cantN;
+        System.out.println("Nombre: "+nombre+", "+"Nota: "+prom);
     }
     
 }
