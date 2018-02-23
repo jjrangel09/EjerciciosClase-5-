1a29c0361f47c1b7f6f568d51a60470539f32ccb
Unknown <karlosdaniel144@gmail.com>
2018-02-23 18:18:04 -0500

    Mes para saber epoca del año

--- EjercicioClase2/src/ejercicioclase2/EjercicioClase2.java
+++ EjercicioClase2/src/ejercicioclase2/EjercicioClase2.java
@@ -4,10 +4,10 @@
  * and open the template in the editor.
  */
 package ejercicioclase2;
-
+import java.util.Scanner;
 /**
  *
- * @author JuanJ
+ * @author JuanJ y CarlosO
  */
 public class EjercicioClase2 {
 
@@ -16,6 +16,54 @@ public class EjercicioClase2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int mes, a=0;
+        while(a!=2){
+        System.out.println("Digite numero del mes: ");
+        mes= sc.nextInt();
+        
+        if(mes==1){
+           System.out.println("Enero-Invierno");  
+        }
+        if(mes==2){
+           System.out.println("Febrero-Invierno");  
+        }
+       if(mes==3){
+           System.out.println("Marzo-Invierno");  
+        }
+        if(mes==4){
+           System.out.println("Abril-Verano");  
+        }
+        if(mes==5){
+           System.out.println("Marzo-Verano");  
+        }
+        if(mes==6){
+           System.out.println("Junio-Verano");  
+        }
+        if(mes==7){
+           System.out.println("Julio-Otoño");  
+        }
+        if(mes==8){
+           System.out.println("Agosto-Otoño");  
+        }
+        if(mes==9){
+           System.out.println("Septiembre-Otoño");  
+        }
+        if(mes==10){
+           System.out.println("Octubre-Primavera");  
+        }
+        if(mes==11){
+           System.out.println("Noviembre-Primavera");  
+        }
+        if(mes==12){
+           System.out.println("Diciembre-Primavera");  
+        }
+        if(mes<=0||mes>=13){
+            System.out.println("Mes no existe");
+        }
+        System.out.println("Desea ingresar otro mes?: Si=1, No=2"); 
+        a = sc.nextInt();
+    }
     }
     
 }
